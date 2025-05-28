14d5bac830b1eec5b29549466bfa57e7bcfb5466
NicolasPafumi <nicolas.pafumi@epfl.ch>
2025-05-28 09:14:35 +0200

    use yesterday for today because the server doesn't give data for current day

--- the_app/lib/provider/stepsProvider.dart
+++ the_app/lib/provider/stepsProvider.dart
@@ -4,11 +4,9 @@ import 'package:the_app/utils/daySteps.dart';
 import 'package:the_app/utils/impact.dart';
 import 'package:intl/intl.dart';
 
-
 class StepsProvider extends ChangeNotifier {
-
   int get todayTotalSteps => _todaySteps.fold(0, (sum, s) => sum + s.value);
-  
+
   List<Steps> _todaySteps = [];
   List<Steps> get todaySteps => _todaySteps;
 
@@ -17,12 +15,13 @@ class StepsProvider extends ChangeNotifier {
 
   Future<void> updateTodaySteps() async {
     final DateTime today = DateTime.now();
-    final String dayStr = DateFormat('yyyy-MM-dd').format(today);
+    final DateTime yesterday = today.subtract(Duration(days: 1)); // use yesterday because data only available the day after
+    final String dayStr = DateFormat('yyyy-MM-dd').format(yesterday);
 
     final steps = await Impact.stepsDuringDay(dayStr);
     _todaySteps = steps ?? [];
 
-    print("updateTodaySteps called, got ${_todaySteps.length} items");//TODO
+    print("updateTodaySteps called, got ${_todaySteps.length} items"); 
     notifyListeners();
   }
 
@@ -30,10 +29,13 @@ class StepsProvider extends ChangeNotifier {
     _stepsEachdays = [];
 
     final DateTime today = DateTime.now();
-    final DateTime startDate = DateTime(today.year, today.month - 2, today.day);
+    final DateTime yesterday = today.subtract(
+      Duration(days: 1),
+    ); // use yesterday because data only available the day after
+    final DateTime startDate = DateTime(yesterday.year, yesterday.month - 2, yesterday.day);
     final formatter = DateFormat('yyyy-MM-dd');
 
-    for (int i = 0; i <= today.difference(startDate).inDays; i++) {
+    for (int i = 0; i <= yesterday.difference(startDate).inDays; i++) {
       final currentDate = startDate.add(Duration(days: i));
       final String dayStr = formatter.format(currentDate);
 
@@ -41,6 +43,8 @@ class StepsProvider extends ChangeNotifier {
       if (stepsList != null && stepsList.isNotEmpty) {
         final int total = stepsList.fold(0, (sum, s) => sum + s.value);
         _stepsEachdays.add(DaySteps(day: currentDate, value: total));
+      } else {
+        _stepsEachdays.add(DaySteps(day: currentDate, value: 0));
       }
     }
 
